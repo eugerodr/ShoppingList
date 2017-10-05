5da6ee0c0a456c1ea58536ac118218173c8323a2
eugenia <eugeniarodriguez.eis@gmail.com>
2017-10-05 16:33:59 +0200

    Layout

---
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.shoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShoppingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+}
